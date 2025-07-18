e5bd83069088461101f1c36de0301c880a5ae149
rafam <rafmor02@ucm.es>
2025-07-18 16:55:50 +0200

    [Test] FIX trasnformer script

--- wasm2wasmMaude_transformer.py
+++ wasm2wasmMaude_transformer.py
@@ -1,7 +1,7 @@
 import argparse
 import re
 
-def transform_file(file, output):
+def transform_file(file, output, func):
     try:
         with open(file, "r", encoding="utf-8", newline="") as f:
             with open(output, "w", encoding="utf-8", newline="") as o:
@@ -18,15 +18,15 @@ def transform_file(file, output):
                     else:
                         transformed_line = re.sub(r'\.', ' . ', line)
                     transformed_line = re.sub(r'\_', '~', transformed_line)
-                    transformed_line = re.sub(r'~i', '~ i', transformed_line)
+                    transformed_line = re.sub(r'~i\d', '~ i', transformed_line)
                     transformed_line = re.sub(r'~f', '~ f', transformed_line)
                     references_re = re.compile('\$(\w+)')
                     references = references_re.findall(transformed_line)
                     for ref in references:
-                        transformed_line = re.sub(rf"\${ref}", rf'"${ref}"', transformed_line)
+                        transformed_line = re.sub(rf"^\"\${ref}", rf'"${ref}"', transformed_line)
                         
                     o.write("\t" + transformed_line + "\n")
-                o.write('\t , "$start") . \n')
+                o.write('\t , "'+func+'") . \n')
                 o.write('endm')
                 o.close()
                 f.close()
@@ -41,6 +41,7 @@ parser = argparse.ArgumentParser(description="Transform WAT to WASMMAUDE syntax"
                                  formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument("-f", "--file", action="store", help="Path to WAT file")
 parser.add_argument("-o", "--output", action="store", help="Path to transformed file")
+parser.add_argument("-e", "--function", action="store", help="Function to invoke. Can be modified later.")
 args = parser.parse_args()
 
-transform_file(args.file, args.output)
+transform_file(args.file, args.output, args.function)
